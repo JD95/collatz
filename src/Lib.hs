045c684786340624d9369dc760fb7ed747ced1c7
Jeffrey Dwyer <jdwyer4@STK.PACIFIC.EDU>
2016-06-27 14:06:25 -0700

    Updated Functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,14 @@ module Lib
     ( someFunc
     ) where
 
+import           Control.Arrow
 import           Data.List
+
+(<&<) :: (a -> Bool) -> (a -> Bool) -> (a -> Bool)
+f <&< g = g &&& f >>> arr (uncurry (&&))
+
+(>&>) :: (a -> Bool) -> (a -> Bool) -> (a -> Bool)
+f >&> g = f &&& g >>> arr (uncurry (&&))
 
 -- | A utility for filtering out fractional numbers
 nonFractional d = (fromIntegral $ ceiling d :: Rational) == d
@@ -19,13 +26,21 @@ collatzIterNIndex :: Rational -> Int -> [(Int, Rational)]
 -- ^ Generates the connecting odds along with their index for base odd b
 collatzIterNIndex b n = filter (nonFractional . snd) $ zip [0..] $ fmap (collatzIterN b n (const 1.0)) [0..]
 
+collatzOddsFromInterN b n =  filter nonFractional . fmap (collatzIterN b n (const 1.0))
+
 -- | Generates an amount of odds from iteration n on the branch with base b
-nCollatzOddsFromIterN b amount n = take amount $ filter nonFractional $ fmap (collatzIterN b n (const 1.0)) [0..]
+nCollatzOddsFromIterN b amount n =  take amount $ collatzOddsFromInterN b n [0..]
 
 -- | Generates the iterative connecting odds for the branch with base b
 nCollatzOdds b amount n = nub $ sort $ concatMap (nCollatzOddsFromIterN b amount) [1..n]
 
-firstOddBase b n = (head . filter ((==) 0 . flip mod 3) . filter nonFractional . fmap (collatzIterN b n (const 1.0))) [0..]
+oddBases b n = filter ((odd . round) <&< nonFractional) . fmap (collatzIterN b n (const 1.0))
+
+firstOddBase b n = (round . head . oddBases b n) [0..]
+
+terminalOdds b n = filter ((==) 0 . flip mod 3 . round) . oddBases b n
+
+firstTerminalOdd b n = (round . head . terminalOdds b n) [0..]
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
